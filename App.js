d35f3145c81c8297eb3486b5571912de70ab29fb
NamanKatariya <dev.works.naman@gmail.com>
2021-04-24 20:39:38 +0530

    parallax Added

--- App.js
+++ App.js
@@ -10,19 +10,21 @@ import CarouselAnimation from './screens/CarouselAnimation';
 import ThreeDCarouselAnimation from './screens/3DCarouselAnimation';
 import TabNavigatorCarousel from './screens/TabNavigatorCarousel';
 import AdvanceFlatlistCarousel from './screens/AdvanceFlatlistCarousel';
+import ParallaxCarousel from './screens/ParallaxCarousel';
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style='auto' />
-      <Stack.Navigator initialRouteName='AdvanceFlatlistCarousel' screenOptions={{ headerShown: false }} >
+      <Stack.Navigator initialRouteName='ParallaxCarousel' screenOptions={{ headerShown: false }} >
         <Stack.Screen name="FlatList" component={FlatListAnimation} />
         <Stack.Screen name="Donut" component={DonutChart} />
         <Stack.Screen name="Carousel" component={CarouselAnimation} />
         <Stack.Screen name="3DCarousel" component={ThreeDCarouselAnimation} />
         <Stack.Screen name="TabNavigatorCarousel" component={TabNavigatorCarousel} />
         <Stack.Screen name="AdvanceFlatlistCarousel" component={AdvanceFlatlistCarousel} />
+        <Stack.Screen name="ParallaxCarousel" component={ParallaxCarousel} />
       </Stack.Navigator>
     </NavigationContainer>
   );
