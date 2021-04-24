196531c8be41c5f16e03ecf16f2a4aa1dd9a5967
NamanKatariya <dev.works.naman@gmail.com>
2021-04-24 15:29:23 +0530

    update

--- App.js
+++ App.js
@@ -9,18 +9,20 @@ import DonutChart from './screens/DonutChart/DonutChart';
 import CarouselAnimation from './screens/CarouselAnimation';
 import ThreeDCarouselAnimation from './screens/3DCarouselAnimation';
 import TabNavigatorCarousel from './screens/TabNavigatorCarousel';
+import AdvanceFlatlistCarousel from './screens/AdvanceFlatlistCarousel';
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style='auto' />
-      <Stack.Navigator initialRouteName='TabNavigatorCarousel' screenOptions={{ headerShown: false }} >
+      <Stack.Navigator initialRouteName='AdvanceFlatlistCarousel' screenOptions={{ headerShown: false }} >
         <Stack.Screen name="FlatList" component={FlatListAnimation} />
         <Stack.Screen name="Donut" component={DonutChart} />
         <Stack.Screen name="Carousel" component={CarouselAnimation} />
         <Stack.Screen name="3DCarousel" component={ThreeDCarouselAnimation} />
         <Stack.Screen name="TabNavigatorCarousel" component={TabNavigatorCarousel} />
+        <Stack.Screen name="AdvanceFlatlistCarousel" component={AdvanceFlatlistCarousel} />
       </Stack.Navigator>
     </NavigationContainer>
   );
